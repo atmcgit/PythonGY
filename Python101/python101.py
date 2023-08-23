bc853cbf3a0f926cfe80a715fc4c7a8ed0612a7f
Mustafa Atmaca <m.atmaca.official@hotmail.com>
2023-08-23 16:14:05 +0300

    substrig

--- Python101/python101.py
+++ Python101/python101.py
@@ -21,7 +21,7 @@ print("a"+"-b")
 
 #STRING METODLARI - LEN
 
-gel_yaz = "geleceği_yazanlar"
+gel_yaz = "gelecegi_yazanlar"
 
 a = 9
 b = 10
@@ -29,6 +29,7 @@ a*b
 print(len(gel_yaz))
 
 #STRING METODLARI - upper() & lower()
+
 B = gel_yaz.upper()
 K = gel_yaz.lower()
 print(B)
@@ -39,8 +40,31 @@ print(K.isupper())
 print(K.islower())
 
 #STRING METODLARI - replace()
+
 gel_yaz.replace("e","a")
 C  = gel_yaz.replace("e","a")
 print(C)
 X = gel_yaz.replace("a","i")
 print(X)
+
+#STRING METODLARI - strip()
+
+# Python'un strip() yöntemi, bir dizenin başındaki ve sonundaki belirli karakterleri kaldırmak için kullanılır. 
+gel_yaz = " gelecegi_yazanlar "
+print(gel_yaz.strip())
+
+gel_yaz= "*gelecegi_yazanlar*"
+print(gel_yaz.strip("*"))
+
+# METODLARA GENEL BAKIŞ
+gel_yaz= "gelecegi_yazanlar"
+print(dir(gel_yaz))
+
+print(gel_yaz.capitalize()) # ilk harf büyüt
+print(gel_yaz.title())      # baş harfleri büyüt
+
+# SUBSTRİNGLER
+gel_yaz = "gelecegi_yazanlar"
+
+gel_yaz[0]
+print(gel_yaz[3:8])
